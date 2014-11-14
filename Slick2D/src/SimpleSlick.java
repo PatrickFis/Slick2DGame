1faa1512208441c26a7d35ddb3d1d3446f3c9392
PatrickFis <fischerpl@mail.lipscomb.edu>
2014-11-13 18:43:32 -0600

    Update SimpleSlick.java

--- Slick2D/src/SimpleSlick.java
+++ Slick2D/src/SimpleSlick.java
@@ -1,18 +1,27 @@
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.geom.Circle;
 
 public class SimpleSlick extends BasicGame
 {
 	SpriteSheet pokemon;
 	Animation pokeAnimation;
+	Circle circle = null;
+	int xpos = 200;
+	int ypos = 200;
+	int radius = 50;
 	public SimpleSlick(String gamename)
 	{
 		super(gamename);
@@ -20,20 +29,46 @@ public class SimpleSlick extends BasicGame
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-		pokemon = new SpriteSheet("res/pokemon.png", 130, 176);
+		pokemon = new SpriteSheet("res/pokemon.png", 120, 176);
 		pokeAnimation = new Animation(pokemon, 100);
+		circle = new Circle(xpos, ypos, radius);
 		}
 
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {
 //		pokeAnimation.update(i);
+		Input input = gc.getInput();
+		if(input.isKeyPressed(Input.KEY_ESCAPE)) {
+			gc.exit();
+		}
+		if(input.isKeyDown(Input.KEY_W)) {
+			circle.setY(ypos -= 10);
+		}
+		if(input.isKeyDown(Input.KEY_S)) {
+			circle.setY(ypos += 10);
+		}
+		if(input.isKeyDown(Input.KEY_A)) {
+			circle.setX(xpos -= 10);
+		}
+		if(input.isKeyDown(Input.KEY_D)) {
+			circle.setX(xpos += 10);
+		}
+		if(input.isKeyDown(Input.KEY_Q)) {
+			circle.setRadius(radius += 10);
+		}
+		if(input.isKeyDown(Input.KEY_E)) {
+			circle.setRadius(radius -= 10);
+		}
 	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-		pokeAnimation.draw(100, 100);
-		pokeAnimation.setSpeed(10000000);
+//		pokeAnimation.draw(100, 100);
+		g.draw(circle);
+//		Random rand = new Random();
+//		Color random = new Color(rand.nextInt(), rand.nextInt(), rand.nextInt());
+//		g.setBackground(random);
 	}
 
 	public static void main(String[] args)
@@ -43,6 +78,10 @@ public class SimpleSlick extends BasicGame
 			AppGameContainer appgc;
 			appgc = new AppGameContainer(new SimpleSlick("Simple Slick Game"));
 			appgc.setDisplayMode(640, 480, false);
+			appgc.setTargetFrameRate(30);
+//			appgc.setVSync(true);
+			appgc.setShowFPS(false);
+//			appgc.setFullscreen(true);
 			appgc.start();
 		}
 		catch (SlickException ex)
@@ -51,12 +90,3 @@ public class SimpleSlick extends BasicGame
 		}
 	}
 }
-
-//class Sprite {
-//	Animation sprite;
-//	Image i1;
-//	Sprite() {
-//		
-//	}
-//	
-//}

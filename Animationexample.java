9927617c2ab88fd52d2a883cc7d06e453135b3d9
bryce350z <bryce350z@users.noreply.github.com>
2014-11-13 18:44:59 -0600

    Update Animationexample.java
    Shows how to add key pressed functions and move basic shapes. Also shows how to use a spritesheet

--- Animationexample.java
+++ Animationexample.java
@@ -1,19 +1,27 @@
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.lwjgl.opengl.Display;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.Renderable;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.geom.Circle;
 
 public class Animationexample extends BasicGame
 {
 	SpriteSheet x;
-	Animation animate;
+	Animation animate = null;
+	Circle circle =  null;
+	int xpos = 200;
+	int ypos = 200;
 	public Animationexample(String gamename)
 	{
 		super(gamename);
@@ -25,20 +33,42 @@ public class Animationexample extends BasicGame
 		SpriteSheet [] sprites = {};
 
 
-		SpriteSheet x = new SpriteSheet("res/sword_man.png",100,103);
+		SpriteSheet x = new SpriteSheet("res/sword.png",80,108);
 		animate = new Animation(x,100);
+		circle = new Circle(xpos,ypos,10);
 	}
 
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException 
 	{
+		Input input = gc.getInput();
+		if(input.isKeyDown(input.KEY_W))
+		{
+			circle.setY(ypos -= 1);
+		}
+		if(input.isKeyDown(input.KEY_S))
+		{
+			circle.setY(ypos += 1);
+		}
+		if(input.isKeyDown(input.KEY_A))
+		{
+			circle.setX(xpos -= 1);
+		}
+		if(input.isKeyDown(input.KEY_D))
+		{
+			circle.setX(xpos += 1);
+		}	
 		animate.update(i);
 	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
+		Input input = gc.getInput();
 		animate.draw(400,400);
+		g.setBackground(Color.red);
+		g.draw(circle);
+	
 	}
 
 	public static void main(String[] args)

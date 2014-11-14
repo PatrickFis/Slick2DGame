e70c4ab614deefc6f45eb4d733d19d231211ab8c
PatrickFis <fischerpl@mail.lipscomb.edu>
2014-11-14 10:41:47 -0600

    Added Animation Movement

--- SimpleSlick.java
+++ SimpleSlick.java
@@ -17,8 +17,9 @@ import org.newdawn.slick.geom.Rectangle;
 
 public class SimpleSlick extends BasicGame
 {
-	SpriteSheet pokemon;
-	Animation pokeAnimation;
+	//	SpriteSheet pokemon;
+	//	Animation pokeAnimation;
+	Player x;
 	Circle circle = null;
 	Rectangle rect = null;
 	int xpos = 200;
@@ -35,65 +36,108 @@ public class SimpleSlick extends BasicGame
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-		pokemon = new SpriteSheet("res/pokemon.png", 120, 176);
-		pokeAnimation = new Animation(pokemon, 100);
+		//		pokemon = new SpriteSheet("res/pokemon.png", 120, 176);
+		//		pokeAnimation = new Animation(pokemon, 100);
+		x = new Player(100, 100);
 		circle = new Circle(xpos, ypos, radius);
 		rect = new Rectangle(rx, ry, rh, rw);
-		}
+	}
 
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {
-//		pokeAnimation.update(i);
+		//		pokeAnimation.update(i);
 		Input input = gc.getInput();
-		if(input.isKeyPressed(Input.KEY_ESCAPE)) {
-			gc.exit();
+		if(!x.isNull()) {
+			if(input.isKeyPressed(Input.KEY_ESCAPE)) gc.exit();
+			if(input.isKeyDown(Input.KEY_W)) {
+				circle.setY(ypos -= 10);
+				x.moveUp(10);
+			}
+			if(input.isKeyDown(Input.KEY_S)) {
+				circle.setY(ypos += 10);
+				x.moveDown(10);
+			}
+			if(input.isKeyDown(Input.KEY_A)) {
+				circle.setX(xpos -= 10);
+				x.moveLeft(10);
+			}
+			if(input.isKeyDown(Input.KEY_D)) {
+				circle.setX(xpos += 10);
+				x.moveRight(10);
+			}
+			if(input.isKeyDown(Input.KEY_Q)) circle.setRadius(radius += 10);
+			if(input.isKeyDown(Input.KEY_E)) circle.setRadius(radius -= 10);
+			if(input.isKeyDown(Input.KEY_EQUALS)) x.destroy();
+			if(circle.intersects(rect)) {
+				if(xpos >= rect.getX()) circle.setX(xpos++);
+				else circle.setX(xpos--);
+				if(ypos >= rect.getY()) circle.setY(ypos++);
+				else circle.setY(ypos--);
+			}
+			if(xpos > 640) xpos = 0;
+			if(ypos > 480) ypos = 0;
+			if(xpos < 0) xpos = 640;
+			if(ypos < 0) ypos = 480;
 		}
-		if(input.isKeyDown(Input.KEY_W)) {
-			circle.setY(ypos -= 10);
+		else {
+			if(input.isKeyPressed(Input.KEY_ESCAPE)) {
+				gc.exit();
+			}
+			if(input.isKeyDown(Input.KEY_W)) {
+				circle.setY(ypos -= 10);
+				x.moveUp(10);
+			}
+			if(input.isKeyDown(Input.KEY_S)) {
+				circle.setY(ypos += 10);
+				x.moveDown(10);
+			}
+			if(input.isKeyDown(Input.KEY_A)) {
+				circle.setX(xpos -= 10);
+				x.moveLeft(10);
+			}
+			if(input.isKeyDown(Input.KEY_D)) {
+				circle.setX(xpos += 10);
+				x.moveRight(10);
+			}
+			if(input.isKeyDown(Input.KEY_Q)) circle.setRadius(radius += 10);
+			if(input.isKeyDown(Input.KEY_E)) circle.setRadius(radius -= 10);
+			if(circle.intersects(rect)) {
+				if(xpos >= rect.getX()) circle.setX(xpos++);
+				else circle.setX(xpos--);
+				if(ypos >= rect.getY()) circle.setY(ypos++);
+				else circle.setY(ypos--);
+			}
+			if(xpos > 640) xpos = 0;
+			if(ypos > 480) ypos = 0;
+			if(xpos < 0) xpos = 640;
+			if(ypos < 0) ypos = 480;
 		}
-		if(input.isKeyDown(Input.KEY_S)) {
-			circle.setY(ypos += 10);
-		}
-		if(input.isKeyDown(Input.KEY_A)) {
-			circle.setX(xpos -= 10);
-		}
-		if(input.isKeyDown(Input.KEY_D)) {
-			circle.setX(xpos += 10);
-		}
-		if(input.isKeyDown(Input.KEY_Q)) {
-			circle.setRadius(radius += 10);
-		}
-		if(input.isKeyDown(Input.KEY_E)) {
-			circle.setRadius(radius -= 10);
-		}
-		if(circle.intersects(rect)) {
-			if(xpos >= rect.getX()) circle.setX(xpos++);
-			else circle.setX(xpos--);
-			if(ypos >= rect.getY()) circle.setY(ypos++);
-			else circle.setY(ypos--);
-		}
-		if(xpos > 640) xpos = 0;
-		if(ypos > 480) ypos = 0;
-		if(xpos < 0) xpos = 640;
-		if(ypos < 0) ypos = 480;
 	}
 
 	@Override
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
-//		pokeAnimation.draw(100, 100);
-//		Random rand = new Random();
-//		Color random = new Color(rand.nextInt(), rand.nextInt(), rand.nextInt());
-//		g.setColor(random);
-//		g.fill(circle);
+		if(!x.isNull()) {
+		x.draw();
+		//		Random rand = new Random();
+		//		Color random = new Color(rand.nextInt(), rand.nextInt(), rand.nextInt());
+		//		g.setColor(random);
+		//		g.fill(circle);
 		g.draw(circle);
 		g.draw(rect);
-//		g.setColor(Color.white);
+		//		g.setColor(Color.white);
 		g.drawString(Float.toString(circle.getX()), 0, 10);
 		g.drawString(Float.toString(circle.getY()), 0, 25);
-//		Random rand = new Random();
-//		Color random = new Color(rand.nextInt(), rand.nextInt(), rand.nextInt());
-//		g.setBackground(random);
+		//		Random rand = new Random();
+		//		Color random = new Color(rand.nextInt(), rand.nextInt(), rand.nextInt());
+		//		g.setBackground(random);
+		}
+		else {
+			g.draw(circle);
+			g.draw(rect);
+			g.drawString(Float.toString(circle.getX()), 0, 10);
+			g.drawString(Float.toString(circle.getY()), 0, 25);
+			}
 	}
 
 	public static void main(String[] args)
@@ -103,10 +147,10 @@ public class SimpleSlick extends BasicGame
 			AppGameContainer appgc;
 			appgc = new AppGameContainer(new SimpleSlick("Simple Slick Game"));
 			appgc.setDisplayMode(640, 480, false);
-			appgc.setTargetFrameRate(30);
-//			appgc.setVSync(true);
+			appgc.setTargetFrameRate(60);
+			//			appgc.setVSync(true);
 			appgc.setShowFPS(false);
-//			appgc.setFullscreen(true);
+			//			appgc.setFullscreen(true);
 			appgc.start();
 		}
 		catch (SlickException ex)
